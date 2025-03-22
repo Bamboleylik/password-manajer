a52a67d885a8173359002fa06f3e925f82b6f59f
Yura <yurchyk2007@gmail.com>
2025-03-22 23:34:40 +0100

    Add project files.

---
+++ password manajer/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace password_manajer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            rbGenerate = new RadioButton();
+            rbCheck = new RadioButton();
+            chkLetters = new CheckBox();
+            chkNumbers = new CheckBox();
+            chkSymbols = new CheckBox();
+            txtPassword = new TextBox();
+            btnGenerate = new Button();
+            btnSave = new Button();
+            numLength = new NumericUpDown();
+            lblStrength = new Label();
+            picStrength = new PictureBox();
+            lstHistory = new ListBox();
+            statusStrip1 = new StatusStrip();
+            statusLabel = new ToolStripStatusLabel();
+            btnCopy = new Button();
+            pbStrength = new ProgressBar();
+            btnDelete = new Button();
+            btnLoad = new Button();
+            ((System.ComponentModel.ISupportInitialize)numLength).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picStrength).BeginInit();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // rbGenerate
+            // 
+            rbGenerate.AutoSize = true;
+            rbGenerate.Location = new Point(795, 36);
+            rbGenerate.Name = "rbGenerate";
+            rbGenerate.Size = new Size(156, 24);
+            rbGenerate.TabIndex = 0;
+            rbGenerate.TabStop = true;
+            rbGenerate.Text = "generate password";
+            rbGenerate.UseVisualStyleBackColor = true;
+            // 
+            // rbCheck
+            // 
+            rbCheck.AutoSize = true;
+            rbCheck.Location = new Point(795, 91);
+            rbCheck.Name = "rbCheck";
+            rbCheck.Size = new Size(187, 24);
+            rbCheck.TabIndex = 1;
+            rbCheck.TabStop = true;
+            rbCheck.Text = "check password stength";
+            rbCheck.UseVisualStyleBackColor = true;
+            // 
+            // chkLetters
+            // 
+            chkLetters.AutoSize = true;
+            chkLetters.Location = new Point(805, 165);
+            chkLetters.Name = "chkLetters";
+            chkLetters.Size = new Size(124, 24);
+            chkLetters.TabIndex = 2;
+            chkLetters.Text = "include letters";
+            chkLetters.UseVisualStyleBackColor = true;
+            // 
+            // chkNumbers
+            // 
+            chkNumbers.AutoSize = true;
+            chkNumbers.Location = new Point(805, 211);
+            chkNumbers.Name = "chkNumbers";
+            chkNumbers.Size = new Size(140, 24);
+            chkNumbers.TabIndex = 3;
+            chkNumbers.Text = "include numbers";
+            chkNumbers.UseVisualStyleBackColor = true;
+            // 
+            // chkSymbols
+            // 
+            chkSymbols.AutoSize = true;
+            chkSymbols.Location = new Point(805, 266);
+            chkSymbols.Name = "chkSymbols";
+            chkSymbols.Size = new Size(191, 24);
+            chkSymbols.TabIndex = 4;
+            chkSymbols.Text = "include specific symbols";
+            chkSymbols.UseVisualStyleBackColor = true;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(248, 33);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(188, 27);
+            txtPassword.TabIndex = 5;
+            // 
+            // btnGenerate
+            // 
+            btnGenerate.Location = new Point(227, 121);
+            btnGenerate.Name = "btnGenerate";
+            btnGenerate.Size = new Size(78, 68);
+            btnGenerate.TabIndex = 6;
+            btnGenerate.Text = "generate";
+            btnGenerate.UseVisualStyleBackColor = true;
+            btnGenerate.Click += btnGenerate_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(346, 121);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(90, 70);
+            btnSave.TabIndex = 7;
+            btnSave.Text = "save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // numLength
+            // 
+            numLength.Location = new Point(805, 324);
+            numLength.Maximum = new decimal(new int[] { 15, 0, 0, 0 });
+            numLength.Name = "numLength";
+            numLength.Size = new Size(146, 27);
+            numLength.TabIndex = 9;
+            // 
+            // lblStrength
+            // 
+            lblStrength.AutoSize = true;
+            lblStrength.Location = new Point(80, 33);
+            lblStrength.Name = "lblStrength";
+            lblStrength.Size = new Size(50, 20);
+            lblStrength.TabIndex = 10;
+            lblStrength.Text = "label1";
+            // 
+            // picStrength
+            // 
+            picStrength.Location = new Point(12, 91);
+            picStrength.Name = "picStrength";
+            picStrength.Size = new Size(209, 229);
+            picStrength.TabIndex = 11;
+            picStrength.TabStop = false;
+            // 
+            // lstHistory
+            // 
+            lstHistory.FormattingEnabled = true;
+            lstHistory.ItemHeight = 20;
+            lstHistory.Location = new Point(227, 248);
+            lstHistory.Name = "lstHistory";
+            lstHistory.Size = new Size(241, 144);
+            lstHistory.TabIndex = 12;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.ImageScalingSize = new Size(20, 20);
+            statusStrip1.Items.AddRange(new ToolStripItem[] { statusLabel });
+            statusStrip1.Location = new Point(0, 424);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(1090, 26);
+            statusStrip1.TabIndex = 13;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            statusLabel.Name = "statusLabel";
+            statusLabel.Size = new Size(151, 20);
+            statusLabel.Text = "toolStripStatusLabel1";
+            // 
+            // btnCopy
+            // 
+            btnCopy.Location = new Point(486, 121);
+            btnCopy.Name = "btnCopy";
+            btnCopy.Size = new Size(90, 70);
+            btnCopy.TabIndex = 14;
+            btnCopy.Text = "copy";
+            btnCopy.UseVisualStyleBackColor = true;
+            btnCopy.Click += btnCopy_Click;
+            // 
+            // pbStrength
+            // 
+            pbStrength.Location = new Point(486, 21);
+            pbStrength.Name = "pbStrength";
+            pbStrength.Size = new Size(213, 39);
+            pbStrength.TabIndex = 15;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(486, 281);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(90, 70);
+            btnDelete.TabIndex = 16;
+            btnDelete.Text = "delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(609, 121);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(90, 70);
+            btnLoad.TabIndex = 17;
+            btnLoad.Text = "load";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1090, 450);
+            Controls.Add(btnLoad);
+            Controls.Add(btnDelete);
+            Controls.Add(pbStrength);
+            Controls.Add(btnCopy);
+            Controls.Add(statusStrip1);
+            Controls.Add(lstHistory);
+            Controls.Add(picStrength);
+            Controls.Add(lblStrength);
+            Controls.Add(numLength);
+            Controls.Add(btnSave);
+            Controls.Add(btnGenerate);
+            Controls.Add(txtPassword);
+            Controls.Add(chkSymbols);
+            Controls.Add(chkNumbers);
+            Controls.Add(chkLetters);
+            Controls.Add(rbCheck);
+            Controls.Add(rbGenerate);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numLength).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picStrength).EndInit();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RadioButton rbGenerate;
+        private RadioButton rbCheck;
+        private CheckBox chkLetters;
+        private CheckBox chkNumbers;
+        private CheckBox chkSymbols;
+        private TextBox txtPassword;
+        private Button btnGenerate;
+        private Button btnSave;
+        private NumericUpDown numLength;
+        private Label lblStrength;
+        private PictureBox picStrength;
+        private ListBox lstHistory;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel statusLabel;
+        private Button btnCopy;
+        private ProgressBar pbStrength;
+        private Button btnDelete;
+        private Button btnLoad;
+    }
+}
---
+++ password manajer/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace password_manajer
+{
+    public partial class Form1 : Form
+    {
+        private string historyFile = "password_history.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadPasswordHistory();
+            statusLabel.Text = "Оберіть режим: генерація або перевірка пароля";
+            picStrength.SizeMode = PictureBoxSizeMode.Zoom;
+            rbGenerate.Checked = true;
+            txtPassword.ReadOnly = true;
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog; 
+            this.Size = new System.Drawing.Size(1000, 600);
+        }
+
+     
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if (rbGenerate.Checked)
+            {
+                string password = GeneratePassword();
+                if (!string.IsNullOrEmpty(password))
+                {
+                    txtPassword.Text = password;
+                    AddToHistory(password);
+                    UpdatePasswordStrength(password);
+                    statusLabel.Text = "Пароль згенеровано!";
+                }
+                txtPassword.ReadOnly = true;
+            }
+            else if (rbCheck.Checked)
+            {
+                string password = txtPassword.Text;
+                UpdatePasswordStrength(password);
+                statusLabel.Text = "Перевірка пароля завершена";
+                txtPassword.ReadOnly = false;
+            }
+        }
+
+        private string GeneratePassword()
+        {
+            const string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string numbers = "0123456789";
+            const string symbols = "!@#$%^&*()-_=+";
+
+            StringBuilder charSet = new StringBuilder();
+            List<char> password = new List<char>();
+            Random rand = new Random();
+
+            if (chkLetters.Checked) charSet.Append(letters);
+            if (chkNumbers.Checked) charSet.Append(numbers);
+            if (chkSymbols.Checked) charSet.Append(symbols);
+
+         
+            if (charSet.Length == 0)
+            {
+                MessageBox.Show("Оберіть хоча б один тип символів!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return "";
+            }
+
+            int length = Convert.ToInt32(numLength.Value);
+            int selectedCategories = (chkLetters.Checked ? 1 : 0) + (chkNumbers.Checked ? 1 : 0) + (chkSymbols.Checked ? 1 : 0);
+
+         
+            if (length < selectedCategories)
+            {
+                MessageBox.Show($"Довжина пароля має бути принаймні {selectedCategories}!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return "";
+            }
+
+           
+            if (chkLetters.Checked) password.Add(letters[rand.Next(letters.Length)]);
+            if (chkNumbers.Checked) password.Add(numbers[rand.Next(numbers.Length)]);
+            if (chkSymbols.Checked) password.Add(symbols[rand.Next(symbols.Length)]);
+
+           
+            for (int i = password.Count; i < length; i++)
+            {
+                password.Add(charSet[rand.Next(charSet.Length)]);
+            }
+
+          
+            password = password.OrderBy(x => rand.Next()).ToList();
+            return new string(password.ToArray());
+        }
+
+        private void UpdatePasswordStrength(string password)
+        {
+            int score = 0;
+            if (password.Length >= 8) score++;
+            if (password.Any(char.IsUpper)) score++;
+            if (password.Any(char.IsLower)) score++;
+            if (password.Any(char.IsDigit)) score++;
+            if (password.Any(ch => "!@#$%^&*()-_=+".Contains(ch))) score++;
+
+            pbStrength.Value = score * 20;
+            pbStrength.ForeColor = GetStrengthColor(score);
+
+           
+            switch (score)
+            {
+                case 1: lblStrength.Text = "Дуже слабкий"; break;
+                case 2: lblStrength.Text = "Слабкий"; break;
+                case 3: lblStrength.Text = "Середній"; break;
+                case 4: lblStrength.Text = "Сильний"; break;
+                case 5: lblStrength.Text = "Дуже сильний"; break;
+                default: lblStrength.Text = "Немає пароля"; break;
+            }
+
+       
+            picStrength.Image = GetStrengthImage(score);
+        }
+
+        private System.Drawing.Color GetStrengthColor(int score)
+        {
+            switch (score)
+            {
+                case 1: return System.Drawing.Color.Red;
+                case 2: return System.Drawing.Color.Orange;
+                case 3: return System.Drawing.Color.Yellow;
+                case 4: return System.Drawing.Color.GreenYellow;
+                case 5: return System.Drawing.Color.Green;
+                default: return System.Drawing.Color.Gray;
+            }
+        }
+
+       
+        private System.Drawing.Image GetStrengthImage(int score)
+        {
+            string[] imagePaths =
+            {
+                "C:\\Users\\asus\\Desktop\\final_project\\password manajer\\password manajer\\very_weak.jpg",
+                "C:\\Users\\asus\\Desktop\\final_project\\password manajer\\password manajer\\weak.png",
+                "C:\\Users\\asus\\Desktop\\final_project\\password manajer\\password manajer\\medium.png",
+                "C:\\Users\\asus\\Desktop\\final_project\\password manajer\\password manajer\\strong.png",
+                "C:\\Users\\asus\\Desktop\\final_project\\password manajer\\password manajer\\very_strong.png"
+            };
+
+            if (score < 1 || score > 5)
+                return null;
+
+            string imagePath = imagePaths[score - 1];
+            return File.Exists(imagePath) ? System.Drawing.Image.FromFile(imagePath) : null;
+        }
+
+        
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (lstHistory.SelectedItem != null)
+            {
+                string selectedPassword = lstHistory.SelectedItem.ToString();
+                Clipboard.SetText(selectedPassword);
+                statusLabel.Text = "Пароль скопійовано з історії в буфер обміну!";
+            }
+            else
+            {
+                MessageBox.Show("Оберіть пароль з історії для копіювання!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+
+        private void AddToHistory(string password)
+        {
+            lstHistory.Items.Add(password);
+            File.AppendAllText(historyFile, password + Environment.NewLine);
+        }
+
+       
+        private void LoadPasswordHistory()
+        {
+            if (File.Exists(historyFile))
+            {
+                string[] passwords = File.ReadAllLines(historyFile);
+                lstHistory.Items.AddRange(passwords);
+            }
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lstHistory.SelectedItem != null)
+            {
+                lstHistory.Items.Remove(lstHistory.SelectedItem);
+                File.WriteAllLines(historyFile, lstHistory.Items.Cast<string>());
+                statusLabel.Text = "Пароль видалено з історії!";
+            }
+            else
+            {
+                MessageBox.Show("Оберіть пароль для видалення!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+     
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*",
+                DefaultExt = "txt",
+                FileName = "password_history.txt"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllLines(saveFileDialog.FileName, lstHistory.Items.Cast<string>());
+                statusLabel.Text = "Паролі збережено у файл!";
+            }
+        }
+
+      
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*",
+                DefaultExt = "txt"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string[] passwords = File.ReadAllLines(openFileDialog.FileName);
+                lstHistory.Items.Clear();
+                lstHistory.Items.AddRange(passwords);
+                statusLabel.Text = "Історію паролів завантажено!";
+            }
+        }
+    }
+}
